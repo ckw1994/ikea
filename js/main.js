2f9797b65091025887590d78b54b60263996b0f8
ckw1994 <97150327+ckw1994@users.noreply.github.com>
2022-03-15 10:37:35 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function(){
+    // mobile - 사이트맵 토글 버튼
+    $('#m-site-map-btn').click(function(){
+        $('#gnb').toggleClass('on');
+    })
+
+// pc - 사이트맵 여/닫기 버튼
+// 열기
+$(function(){
+    $('#pc-site-map-btn').click(function(){
+        $('.site-map-modal').addClass('on');
+    })
+})
+// 닫기
+$(function(){
+    $('#close-btn').click(function(){
+        $('.site-map-modal').removeClass('on');
+    })
+})
+
+})
+
